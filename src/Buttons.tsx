d50aa0301bb954d46987c92067cb644f011f4fb0
Raw Art <theartintheraw@gmail.com>
2022-07-05 12:51:01 -0400

    button

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -1,10 +1,25 @@
-import Button from '@material-ui/core/Button';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import styled from "@emotion/styled";
 
-export default function ContainedButtons()
-{
-    return (
-        <Button variant="contained" href="https://alottasol.com">
-            Home
-        </Button>
-    );
+const CTAButton = styled(Button)`
+  width: 200px;
+  height: 60px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export default function ContainedButtons() {
+  return (
+
+
+      <CTAButton variant="contained" href="https://alottasol.com" >
+        Home
+      </CTAButton>
+
+  );
 }
